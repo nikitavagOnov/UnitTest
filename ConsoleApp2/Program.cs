f0dfbfa2ac9167c4ed7bf37dae4995f829bd40d5
нахуй 11 винду < 11 @KOMPUTER>
2023-04-16 14:06:16 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[,] array = { { 2, 3, 5 }, { 7, 2, 1 } };
+            //Method(array, 5);
+            //Console.ReadKey();
+        }
+
+        public static int[,] Method(int[,] array, int value)
+        {
+            int[,] result = new int[array.GetLength(0), array.GetLength(1)];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[i, j] == value)
+                    {
+                        result[i, j] = value;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ConsoleApp2;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[,] array = { { 2, 3, 5 }, { 7, 2, 1 } };
+            
+
+            int[,] actual = Program.Method(array, 5);
+            int[,] expected = { { 0, 0, 5 }, { 0, 0, 0 } };
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
